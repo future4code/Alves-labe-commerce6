205195a0834c9395905e7cd083834aedc0314267
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-06-18 17:50:58 -0300

    carrinho

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import Home from "./components/Home";
 import Carrinho from "./components/Carrinho";
 import Filtro from "./components/Filtro";
+
+
+
+
+
+
 export default class App extends React.Component {
   render() {
     return (
